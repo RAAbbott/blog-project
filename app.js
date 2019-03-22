75f70b0a5a104e7d737b9dd140dbba1dd3e35d4d
Alex Abbott <41922804+RAAbbott@users.noreply.github.com>
2019-03-21 22:42:50 -0600

    Added database to store posts using mongodb/mongoose

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 //jshint esversion:6
+//jslint es6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const posts = [];
+mongoose.connect("mongodb+srv://admin-alex:08251996admin@cluster0-dvf4b.mongodb.net/blogDB", { useNewUrlParser: true });
+
+const postSchema = new mongoose.Schema ({
+  title: {
+    type: String,
+    required: [true, "Blog Title is required..."]
+  },
+  body: {
+    type: String,
+    required: [true, "Body is required..."]
+  }
+});
+
+const Post = mongoose.model("Post", postSchema);
+
 
 const homeStartingContent = "Welcome to this Daily Journal/Blog app! Take some time to write down your thoughts about whatever is on your mind at the moment. Writing is a great way to relieve stress and provoke deep thought. If you are struggling with a question or are anxious about something, write it down, express how you feel and you'll feel better! Just click on the NEW ENTRY option at the top to get started. Anyways, thanks for stumbling on this page, and I hope you enjoy the app! (Notes are saved for as long as you are on the page, after you leave or reload the page they disappear).";
 const aboutContent = "There isn't much to see here, this is mostly a filler page. If I were a blogger I'd have a ong story introducing myself, what I like to do and why I'm entering the blogging space. I'd probably have some professional pictures posted here as well.";
@@ -20,7 +36,14 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res) {
-  res.render("home", {homeContent: homeStartingContent, posts: posts});
+  Post.find(function(err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {homeContent: homeStartingContent, posts: posts});
+    }
+  }) 
+  
 });
 
 app.get("/about", function(req, res) {
@@ -36,21 +59,34 @@ app.get("/compose", function(req, res) {
 });
 
 app.get("/post/:postTitle", function(req, res){
-  const singlePost = posts.filter(post => _.lowerCase(post.title) == _.lowerCase(req.params.postTitle));
-  res.render("post", {title: singlePost[0].title, body: singlePost[0].body, postTitle: req.params.postTitle});
+  Post.findOne({title: req.params.postTitle}, function(err, post) {
+    if (err) {
+      console.log(error);
+    } else {
+      res.render("post", {title: post.title, body: post.body, postTitle: req.params.postTitle});
+    }
+  })
 });
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const newPost = new Post({
     title: req.body.entryTitle,
     body: req.body.entryBody,
-  };
-  posts.push(post);
+  });
+
+  newPost.save();
 
   res.redirect("/");
 });
 
-
+app.post("/delete", function(req, res) {
+  Post.deleteOne({_id: req.body.hiddenInput}, function(err) {
+    if (err) console.log(err);
+    else {
+      res.redirect("/");
+    }
+  });
+});
 
 
 
